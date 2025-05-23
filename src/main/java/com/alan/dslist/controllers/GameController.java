c31908842062c01287455dc328b2d08e1ed5a7e1
AlansidneyJunior <alansidney.silva21@gmail.com>
2025-05-22 21:40:17 -0300

    Implementing FindById

--- src/main/java/com/alan/dslist/controllers/GameController.java
+++ src/main/java/com/alan/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alan.dslist.dto.GameDTO;
 import com.alan.dslist.dto.GameMinDTO;
 import com.alan.dslist.services.GameService;
 
@@ -16,10 +18,15 @@ public class GameController {
 
 	@Autowired
 	private GameService gameService;
-	
+
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		return gameService.findById(id);
+	}
+
 	@GetMapping
-	public List<GameMinDTO> findAll(){
+	public List<GameMinDTO> findAll() {
 		return gameService.findAll();
 	}
-	
+
 }
--- src/main/java/com/alan/dslist/services/GameService.java
+++ src/main/java/com/alan/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.alan.dslist.dto.GameDTO;
 import com.alan.dslist.dto.GameMinDTO;
 import com.alan.dslist.entities.Game;
 import com.alan.dslist.repositories.GameRepository;
@@ -15,6 +17,12 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game game = gameRepository.findById(id).get();
+		return new GameDTO(game);
+	}
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> gameList = gameRepository.findAll();
 		return gameList.stream().map(game -> new GameMinDTO(game)).toList();
